7d2b0e33f35ed6490ddb079fa19a3cd93fa82937
Juma Allan <allanjuma@gmail.com>
2020-11-17 12:15:34 +0300

    setting up Network utils

---
+++ core/src/main/java/com/safeboda/core/network/AuthInterceptor.kt
@@ -0,0 +1,18 @@
+package com.safeboda.core.network
+
+import com.safeboda.core.settings.Settings
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthInterceptor(
+    private val settings: Settings,
+) : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        var chainRequest = chain.request()
+        chainRequest =
+            chainRequest.newBuilder().header("Authorization", settings.bearerToken.orEmpty())
+                .build()
+        return chain.proceed(chainRequest)
+    }
+}
