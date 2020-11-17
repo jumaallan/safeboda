071b50cdec50c231af87561a06a7fde74c610035
Juma Allan <allanjuma@gmail.com>
2020-11-17 12:12:57 +0300

    added core module

---
+++ core/build.gradle.kts
@@ -0,0 +1,73 @@
+plugins {
+    id(BuildPlugins.androidLibrary)
+    id(BuildPlugins.kotlinAndroid)
+    id(BuildPlugins.kotlinAndroidExtensions)
+    id(BuildPlugins.kapt)
+}
+
+android {
+    compileSdkVersion(AndroidSdk.compileSdkVersion)
+
+    defaultConfig {
+        minSdkVersion(AndroidSdk.minSdkVersion)
+        targetSdkVersion(AndroidSdk.targetSdkVersion)
+        vectorDrawables.useSupportLibrary = true
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildTypes {
+        getByName("debug") {
+            isDebuggable = true
+        }
+
+        getByName("release") {
+            isMinifyEnabled = true
+        }
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation(Libraries.kotlinStdLib)
+    implementation(Libraries.coreKtx)
+
+    // Material Design
+    implementation(Libraries.material)
+
+    // Firebase crashlytics
+    implementation(Libraries.crashlytics)
+
+    // Network - Retrofit, OKHTTP
+    implementation(Libraries.retrofit)
+    implementation(Libraries.gson)
+
+    // Room
+    implementation(Libraries.room)
+    implementation(Libraries.roomRuntime)
+    kapt(Libraries.roomCompiler)
+
+    // Coroutines
+    implementation(Libraries.coroutines)
+    implementation(Libraries.coroutinesAndroid)
+
+    // DI - KOIN
+    implementation(Libraries.koin)
+    implementation(Libraries.koinViewModel)
+
+    // debug
+    implementation(Libraries.timber)
+
+    // tests
+    testImplementation(TestLibraries.jUnit)
+    androidTestImplementation(TestLibraries.jUnitTest)
+    testImplementation(TestLibraries.truth)
+}
