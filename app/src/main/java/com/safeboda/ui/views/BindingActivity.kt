7024ea637e131a41d6dc3dce2c9d7b984baf0f7e
Juma Allan <allanjuma@gmail.com>
2020-11-17 18:51:05 +0300

    a little housekeeping

--- app/src/main/java/com/safeboda/ui/views/BindingActivity.kt
+++ app/src/main/java/com/safeboda/ui/views/BindingActivity.kt
@@ -1,13 +1,14 @@
 package com.safeboda.ui.views
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 
 /**
  * Abstract Activity to create a [ViewDataBinding] for the given type when setting the content view.
  */
-abstract class BindingActivity<T : ViewDataBinding> : SafebodaActivity() {
+abstract class BindingActivity<T : ViewDataBinding> : AppCompatActivity() {
 
     abstract val layoutResId: Int
 
@@ -25,17 +26,4 @@ abstract class BindingActivity<T : ViewDataBinding> : SafebodaActivity() {
         super.onDestroy()
     }
 
-//    internal fun initAppBarLayout(title: String? = null, subtitle: String? = null) {
-//        dataBinding.root.findViewById<AppBarLayout>(R.id.app_bar_layout)?.let { appBarLayout ->
-//            appBarLayout.title?.text = title ?: ""
-//            appBarLayout.subtitle?.text = subtitle ?: ""
-//            appBarLayout.subtitle?.visibility = if (subtitle.isNullOrBlank()) GONE else VISIBLE
-//            val backIcon =
-//                ContextCompat.getDrawable(this, R.drawable.ic_arrow_left_24)?.mutate()?.also {
-//                    it.setTint(ContextCompat.getColor(this, R.color.textPrimary))
-//                }
-//            appBarLayout.toolbar?.navigationIcon = backIcon
-//            appBarLayout.toolbar?.setNavigationOnClickListener { onBackPressed() }
-//        }
-//    }
 }
