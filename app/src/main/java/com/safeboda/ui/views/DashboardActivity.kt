b6807bbb4d862380839b0ee56abaf29b177a409f
Juma Allan <allanjuma@gmail.com>
2020-11-18 01:46:31 +0300

    mock data on the dashboard

--- app/src/main/java/com/safeboda/ui/views/DashboardActivity.kt
+++ app/src/main/java/com/safeboda/ui/views/DashboardActivity.kt
@@ -1,16 +1,28 @@
 package com.safeboda.ui.views
 
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout.OnRefreshListener
 import com.safeboda.R
+import com.safeboda.core.data.models.UserOrOrganization
+import com.safeboda.core.data.remote.UserOrganizationRepository
+import com.safeboda.core.network.ApiFailure
+import com.safeboda.core.network.ApiFailureType
 import com.safeboda.core.span.LabelColor
+import com.safeboda.core.utils.toast
 import com.safeboda.databinding.ActivityDashboardBinding
 import com.safeboda.ui.interfaces.OnUserSelectedListener
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import org.koin.android.ext.android.inject
+import timber.log.Timber
 
 class DashboardActivity :
     BindingActivity<ActivityDashboardBinding>(),
     OnUserSelectedListener,
     OnRefreshListener {
+
+    private val userOrganizationRepository: UserOrganizationRepository by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +30,32 @@ class DashboardActivity :
 
         binding.statusBackground.background =
             LabelColor.backgroundDrawable(this, LabelColor.GRAY)
+
+        lifecycleScope.launch {
+            userOrganizationRepository.fetchUserOrOrganization("jumaallan") {
+                toast("failed")
+            }.collect { profile ->
+                if (profile != null) {
+                    handleProfileSuccess(profile)
+                    binding.headerItem = profile
+                } else {
+                    handleProfileFailure(
+                        ApiFailure(ApiFailureType.PARSE_ERROR, null, null)
+                    )
+                }
+            }
+        }
+
+    }
+
+    private fun handleProfileSuccess(profile: UserOrOrganization) {
+        Timber.d("Updating user profile list items.")
+    }
+
+    private fun handleProfileFailure(
+        failure: ApiFailure
+    ) {
+        Timber.d("Failed to fetch profile due to $failure")
     }
 
     override val layoutResId: Int
