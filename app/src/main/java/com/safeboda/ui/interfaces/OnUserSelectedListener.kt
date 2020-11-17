eaad3c427e399a67b88109515bf38befe9c15ce1
Juma Allan <allanjuma@gmail.com>
2020-11-17 19:37:55 +0300

    added user data class and selected interface

---
+++ app/src/main/java/com/safeboda/ui/interfaces/OnUserSelectedListener.kt
@@ -0,0 +1,5 @@
+package com.safeboda.ui.interfaces
+
+interface OnUserSelectedListener {
+    fun onUserOrOrgSelected(login: String)
+}
---
+++ app/src/main/java/com/safeboda/ui/model/UserItem.kt
@@ -0,0 +1,9 @@
+package com.safeboda.ui.model
+
+data class UserItem(
+    val id: String,
+    val name: String?,
+    val login: String,
+    val bioHtml: String,
+    val avatarUrl: String
+)
