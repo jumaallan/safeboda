28183fe7fc6b85fe85ec3deb36c36bb955395e54
Juma Allan <allanjuma@gmail.com>
2020-11-17 18:49:22 +0300

    refactoring app dashboard

--- app/src/main/java/com/safeboda/ui/views/DashboardActivity.kt
+++ app/src/main/java/com/safeboda/ui/views/DashboardActivity.kt
@@ -4,10 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.safeboda.R
 
-class MainActivity : AppCompatActivity() {
+class DashboardActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_dashboard)
     }
 }
