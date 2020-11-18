fb1e6b95c6e20e094e4cbea36ab0c4e8675f5dae
Juma Allan <allanjuma@gmail.com>
2020-11-18 16:58:15 +0300

    cleaning up app dashboard

--- app/src/main/java/com/safeboda/ui/activity/DashboardActivity.kt
+++ app/src/main/java/com/safeboda/ui/activity/DashboardActivity.kt
@@ -7,47 +7,62 @@ import com.safeboda.R
 import com.safeboda.core.network.ApiModel
 import com.safeboda.databinding.ActivityDashboardBinding
 import com.safeboda.ui.adapters.UserOrOrganizationAdapter
+import com.safeboda.ui.adapters.UsersAdapter
 import com.safeboda.ui.base.BindingActivity
-import com.safeboda.ui.interfaces.OnUserSelectedListener
+import com.safeboda.ui.scroller.PaginatedScrollListener
+import com.safeboda.ui.viewmodel.FollowingUsersViewModel
 import com.safeboda.ui.viewmodel.UserOrganizationViewModel
 import com.safeboda.ui.viewmodel.UserOrganizationViewModel.ListItemProfile
+import com.safeboda.ui.viewmodel.UsersViewModel.ListItemUser
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import timber.log.Timber
 
 class DashboardActivity :
     BindingActivity<ActivityDashboardBinding>(),
-    OnUserSelectedListener,
     OnRefreshListener {
 
     private val userOrganizationViewModel: UserOrganizationViewModel by viewModel()
-    private lateinit var adapter: UserOrOrganizationAdapter
+    private val usersViewModel: FollowingUsersViewModel by viewModel()
+
+    private lateinit var userOrOrganizationAdapter: UserOrOrganizationAdapter
+    private lateinit var usersAdapter: UsersAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.lifecycleOwner = this
 
         userOrganizationViewModel.profileModel.observe(this, Observer(::onUserModelChanged))
+        usersViewModel.userModel.observe(this, { onUserFollowersFollowingModelChanged(it) })
 
-        adapter = UserOrOrganizationAdapter(this)
-        binding.viewUserOrgProfile.recyclerView?.adapter = adapter
+        userOrOrganizationAdapter = UserOrOrganizationAdapter(this)
+        binding.viewUserOrgProfile.recyclerView?.adapter = userOrOrganizationAdapter
         binding.viewUserOrgProfile.enableSwipeToRefresh(this)
+
+        usersAdapter = UsersAdapter { user ->
+            userOrganizationViewModel.fetchUserOrOrganization(user.login, user.avatarUrl, user.name)
+        }
+        binding.recyclerViewFollowersFollowing.adapter = usersAdapter
+        binding.recyclerViewFollowersFollowing.addOnScrollListener(
+            PaginatedScrollListener(
+                usersViewModel
+            )
+        )
 
         onRefresh()
     }
 
     private fun onUserModelChanged(model: ApiModel<List<ListItemProfile>>) {
-        adapter.setData(model.data)
+        userOrOrganizationAdapter.setData(model.data)
         binding.viewUserOrgProfile.showViewForApiStatus(model, this)
+    }
+
+    private fun onUserFollowersFollowingModelChanged(model: ApiModel<List<ListItemUser>>) {
+        usersAdapter.submitList(model.data)
     }
 
     override val layoutResId: Int
         get() = R.layout.activity_dashboard
 
-    override fun onUserOrOrgSelected(login: String) {
-        Timber.d("the user or org selected is $login")
-    }
-
     override fun onRefresh() {
-        userOrganizationViewModel.fetchUserOrOrganization("android254")
+        userOrganizationViewModel.fetchUserOrOrganization("jumaallan", "", "")
     }
 }
