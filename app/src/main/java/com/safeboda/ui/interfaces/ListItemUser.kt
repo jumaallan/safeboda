4600c8178ad02c29ff5735d8b94ef53f95a7ba73
Juma Allan <allanjuma@gmail.com>
2020-11-18 16:59:46 +0300

    setting up user viewmodel

---
+++ app/src/main/java/com/safeboda/ui/interfaces/ListItemUser.kt
@@ -0,0 +1,9 @@
+package com.safeboda.ui.interfaces
+
+interface ListItemUser {
+    val id: String
+    val name: String?
+    val login: String
+    val bioHtml: String
+    val avatarUrl: String
+}
