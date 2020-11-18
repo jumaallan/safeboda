f9ea12c318bde06d3deb91e97f65ba12ad39b42b
Juma Allan <allanjuma@gmail.com>
2020-11-18 16:57:51 +0300

    remove unused interfaces

--- app/src/main/java/com/safeboda/ui/interfaces/OnProfileItemListener.kt
+++
@@ -1,5 +0,0 @@
-package com.safeboda.ui.interfaces
-
-interface OnProfileItemListener {
-    fun onOrganizationsSelected(login: String)
-}
--- app/src/main/java/com/safeboda/ui/interfaces/OnUserSelectedListener.kt
+++
@@ -1,5 +0,0 @@
-package com.safeboda.ui.interfaces
-
-interface OnUserSelectedListener {
-    fun onUserOrOrgSelected(login: String)
-}
