70e7112ea7b5ca350e95c6993dde93882f4098eb
Juma Allan <allanjuma@gmail.com>
2020-11-18 12:49:04 +0300

    cleaning up app dashboard

--- app/src/main/java/com/safeboda/ui/views/DashboardActivity.kt
+++ app/src/main/java/com/safeboda/ui/views/DashboardActivity.kt
@@ -1,20 +1,18 @@
 package com.safeboda.ui.views
 
 import android.os.Bundle
-import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout.OnRefreshListener
 import com.safeboda.R
-import com.safeboda.core.data.models.UserOrOrganization
-import com.safeboda.core.data.remote.UserOrganizationRepository
-import com.safeboda.core.network.ApiFailure
-import com.safeboda.core.network.ApiFailureType
-import com.safeboda.core.span.LabelColor
-import com.safeboda.core.utils.toast
+import com.safeboda.core.network.ApiModel
 import com.safeboda.databinding.ActivityDashboardBinding
+import com.safeboda.ui.adapters.UserOrOrganizationAdapter
 import com.safeboda.ui.interfaces.OnUserSelectedListener
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
-import org.koin.android.ext.android.inject
+import com.safeboda.ui.viewmodel.UserOrganizationViewModel
+import com.safeboda.ui.viewmodel.UserOrganizationViewModel.ListItemProfile
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import timber.log.Timber
 
 class DashboardActivity :
@@ -22,49 +20,38 @@ class DashboardActivity :
     OnUserSelectedListener,
     OnRefreshListener {
 
-    private val userOrganizationRepository: UserOrganizationRepository by inject()
+    private val userOrganizationViewModel: UserOrganizationViewModel by viewModel()
+    private lateinit var adapter: UserOrOrganizationAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.lifecycleOwner = this
 
-        binding.statusBackground.background =
-            LabelColor.backgroundDrawable(this, LabelColor.GRAY)
+        userOrganizationViewModel.profileModel.observe(this, Observer(::onUserModelChanged))
 
-        lifecycleScope.launch {
-            userOrganizationRepository.fetchUserOrOrganization("jumaallan") {
-                toast("failed")
-            }.collect { profile ->
-                if (profile != null) {
-                    handleProfileSuccess(profile)
-                    binding.headerItem = profile
-                } else {
-                    handleProfileFailure(
-                        ApiFailure(ApiFailureType.PARSE_ERROR, null, null)
-                    )
-                }
-            }
-        }
+        adapter = UserOrOrganizationAdapter(this)
+        binding.viewFlipper.recyclerView?.layoutManager =
+            LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        binding.viewFlipper.recyclerView?.adapter = adapter
+        binding.viewFlipper.enableSwipeToRefresh(this)
+
+        onRefresh()
+
     }
 
-    private fun handleProfileSuccess(profile: UserOrOrganization) {
-        Timber.d("Updating user profile list items.")
-    }
-
-    private fun handleProfileFailure(
-        failure: ApiFailure
-    ) {
-        Timber.d("Failed to fetch profile due to $failure")
+    private fun onUserModelChanged(model: ApiModel<List<ListItemProfile>>) {
+        adapter.setData(model.data)
+        binding.viewFlipper.showViewForApiStatus(model, this)
     }
 
     override val layoutResId: Int
         get() = R.layout.activity_dashboard
 
     override fun onUserOrOrgSelected(login: String) {
-        TODO("Not yet implemented")
+        Timber.d("the user or org selected is $login")
     }
 
     override fun onRefresh() {
-        TODO("Not yet implemented")
+        userOrganizationViewModel.fetchUserOrOrganization("android254")
     }
 }
