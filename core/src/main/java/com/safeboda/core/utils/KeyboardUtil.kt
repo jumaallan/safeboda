ef5b5f660c3be7664e057ab5b05dfb985e5cf45d
Juma Allan <allanjuma@gmail.com>
2020-11-19 18:47:15 +0300

    added keyboard extension function

---
+++ core/src/main/java/com/safeboda/core/utils/KeyboardUtil.kt
@@ -0,0 +1,17 @@
+package com.safeboda.core.utils
+
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.view.inputmethod.InputMethodManager.HIDE_IMPLICIT_ONLY
+import android.view.inputmethod.InputMethodManager.SHOW_FORCED
+import android.widget.EditText
+
+fun View.hideKeyboard() {
+    context?.let {
+        val inputMethodManager =
+            it.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(windowToken, 0)
+    }
+}
