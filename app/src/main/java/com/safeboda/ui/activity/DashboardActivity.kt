02601a25fd4b0184428c009a2c5c32a945fd847c
Juma Allan <allanjuma@gmail.com>
2020-11-19 18:46:23 +0300

    cleaning up the app dashboard

--- app/src/main/java/com/safeboda/ui/activity/DashboardActivity.kt
+++ app/src/main/java/com/safeboda/ui/activity/DashboardActivity.kt
@@ -16,32 +16,43 @@
 package com.safeboda.ui.activity
 
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.Observer
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout.OnRefreshListener
 import com.safeboda.R
 import com.safeboda.core.network.ApiModel
+import com.safeboda.core.utils.hideKeyboard
 import com.safeboda.databinding.ActivityDashboardBinding
 import com.safeboda.ui.adapters.UserOrOrganizationAdapter
 import com.safeboda.ui.base.BindingActivity
+import com.safeboda.ui.interfaces.OnBackPressedListener
+import com.safeboda.ui.interfaces.OnUserOrOrganizationSelectedListener
 import com.safeboda.ui.viewmodel.UserOrganizationViewModel
 import com.safeboda.ui.viewmodel.UserOrganizationViewModel.ListItemProfile
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class DashboardActivity :
     BindingActivity<ActivityDashboardBinding>(),
-    OnRefreshListener {
+    OnRefreshListener,
+    OnUserOrOrganizationSelectedListener,
+    OnBackPressedListener {
 
     private val userOrganizationViewModel: UserOrganizationViewModel by viewModel()
 
     private lateinit var userOrOrganizationAdapter: UserOrOrganizationAdapter
-//    private lateinit var usersAdapter: UsersAdapter
+
+    private var githubUsername: String = ""
+
+    private lateinit var searchView: SearchView
+    private lateinit var searchItem: MenuItem
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.lifecycleOwner = this
 
-//        binding.appBarLayout.title.text = getString(R.string.menu_search)
-//        binding.appBarLayout.toolbar.inflateMenu(R.menu.menu_search)
+        binding.appBarLayout.title.text = getString(R.string.menu_search)
+        binding.appBarLayout.toolbar.inflateMenu(R.menu.menu_search)
 
         userOrganizationViewModel.profileModel.observe(this, Observer(::onUserModelChanged))
 
@@ -50,6 +61,25 @@ class DashboardActivity :
         binding.viewUserOrgProfile.enableSwipeToRefresh(this)
 
         onRefresh()
+
+        searchItem = binding.appBarLayout.toolbar.menu.findItem(R.id.search_item)
+        searchView = searchItem.actionView as SearchView
+        searchView.queryHint = resources.getString(R.string.menu_search)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                userOrganizationViewModel.fetchUserOrOrganization(query.toString())
+                githubUsername = query.toString()
+                searchView.hideKeyboard()
+                searchItem.collapseActionView()
+                binding.viewUserOrgProfile.showLoading()
+                return true
+            }
+        })
+
     }
 
     private fun onUserModelChanged(model: ApiModel<List<ListItemProfile>>) {
@@ -61,6 +91,28 @@ class DashboardActivity :
         get() = R.layout.activity_dashboard
 
     override fun onRefresh() {
-        userOrganizationViewModel.fetchUserOrOrganization("jumaallan", "", "")
+        when {
+            githubUsername.isNotEmpty() -> {
+                userOrganizationViewModel.fetchUserOrOrganization(githubUsername)
+            }
+            else -> {
+                binding.viewUserOrgProfile.showEmpty()
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        when {
+            searchItem.isActionViewExpanded -> {
+                searchItem.collapseActionView()
+            }
+        }
+    }
+
+    override fun onUserOrOrgSelected(login: String) {
+        userOrganizationViewModel.fetchUserOrOrganization(login)
+        githubUsername = login
+        binding.viewUserOrgProfile.showLoading()
     }
 }
