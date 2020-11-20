d1eae66975a1fa4859f4e5c052f426daf77d41b0
Juma Allan <allanjuma@gmail.com>
2020-11-21 01:29:11 +0300

    added core base test

---
+++ core/src/test/java/com/safeboda/core/BaseTest.kt
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2020 Safeboda
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.safeboda.core
+
+import com.apollographql.apollo.ApolloClient
+import com.safeboda.core.dispatcher.MockRequestDispatcher
+import com.safeboda.core.utils.Constants
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.After
+import org.junit.Before
+import java.io.IOException
+import java.util.concurrent.TimeUnit
+
+open class BaseTest : BaseKoinTest() {
+
+    // mock web server and apollo graphQL
+    private lateinit var mockWebServer: MockWebServer
+    private lateinit var okHttpClient: OkHttpClient
+    private lateinit var loggingInterceptor: HttpLoggingInterceptor
+    lateinit var apollo: ApolloClient
+
+    @Before
+    fun setup() {
+
+        mockWebServer = MockWebServer()
+        mockWebServer.dispatcher = MockRequestDispatcher()
+        mockWebServer.start()
+        loggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        okHttpClient = buildOkhttpClient(loggingInterceptor)
+
+        apollo = ApolloClient.builder()
+            .serverUrl(Constants.BASE_URL)
+            .okHttpClient(okHttpClient)
+            .build()
+    }
+
+    @After
+    @Throws(IOException::class)
+    open fun tearDown() {
+        mockWebServer.shutdown()
+    }
+
+    private fun buildOkhttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .readTimeout(60, TimeUnit.SECONDS)
+            .build()
+    }
+}
