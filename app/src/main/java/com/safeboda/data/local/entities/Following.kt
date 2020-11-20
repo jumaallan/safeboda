fd118c5e8a9f849de50c4f0c2cc5b89fc13fd2c9
Juma Allan <allanjuma@gmail.com>
2020-11-20 12:07:13 +0300

    setting up following dao and entity

---
+++ app/src/main/java/com/safeboda/data/local/entities/Following.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2020 Safeboda
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.safeboda.data.local.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Following(
+    @PrimaryKey(autoGenerate = true)
+    var id: Long,
+    val userLogin: String,
+    val login: String,
+    val name: String,
+    val avatarUrl: String,
+    val bioHtml: String,
+)
