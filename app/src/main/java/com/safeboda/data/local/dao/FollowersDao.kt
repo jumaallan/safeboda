34032d2780a3b7b7872fb88f68667e15c29b0149
Juma Allan <allanjuma@gmail.com>
2020-11-20 12:06:36 +0300

    setting up followers dao and entity

---
+++ app/src/main/java/com/safeboda/data/local/dao/FollowersDao.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2020 Safeboda
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.safeboda.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Query
+import com.safeboda.data.local.entities.Followers
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface FollowersDao : BaseDao<Followers> {
+
+    @Query("SELECT * FROM Followers WHERE userLogin =:userLogin")
+    fun getFollowersByGithubUsername(userLogin: String): Flow<List<Followers>>
+
+}
