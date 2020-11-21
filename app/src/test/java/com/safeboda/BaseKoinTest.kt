3bf4bd1f4ea0da61017764d8ce33d77649de8d41
Juma Allan <allanjuma@gmail.com>
2020-11-21 16:25:25 +0300

    cleaning up tests

--- app/src/test/java/com/safeboda/BaseKoinTest.kt
+++ app/src/test/java/com/safeboda/BaseKoinTest.kt
@@ -15,24 +15,12 @@
  */
 package com.safeboda
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import com.safeboda.util.CoroutineTestRule
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import com.safeboda.util.KoinTestRule
 import org.junit.Rule
+import org.koin.test.KoinTest
 
-abstract class BaseViewModelTest : BaseKoinTest() {
+abstract class BaseKoinTest : KoinTest {
 
-    /**
-     * Swaps the background executor used by the Architecture Components with a different one which
-     * executes each task synchronously.
-     **/
     @get:Rule
-    open val instantExecutorRule = InstantTaskExecutorRule()
-
-    /**
-     * A test rule to allow testing coroutines that use the main dispatcher
-     */
-    @ExperimentalCoroutinesApi
-    @get:Rule
-    open val coroutineTestRule = CoroutineTestRule()
+    val koinTestRule = KoinTestRule()
 }
