b8df2ca0c351b230b6bd4dfcbb91377121a62c68
Juma Allan <allanjuma@gmail.com>
2020-11-22 10:24:25 +0300

    added named arguements to sample data

--- app/src/test/java/com/safeboda/data/local/sample/SampleData.kt
+++ app/src/test/java/com/safeboda/data/local/sample/SampleData.kt
@@ -25,51 +25,51 @@ const val name = "Kabiru Mwenja"
 val testUser = listOf(
     User(
         0,
-        "https://github.com/jumaallan",
-        "https://avatars2.githubusercontent.com/u/25085146?u=ce5247f3c267df4cdbe2af79efee9c848d1791e0&v=4",
-        "<div>Android @ Backbase</div>",
-        "<div>Backbase</div>",
-        "allanjuma@gmail.com",
-        610,
-        13,
-        false,
-        true,
-        false,
-        false,
-        "Nairobi",
-        "jumaallan",
-        "Juma Allan",
-        16,
-        45,
-        108,
-        false,
-        false,
-        "https://androidstudy.com",
-        false,
-        "<div><g-emoji class=\"g-emoji\" alias=\"beach_umbrella\" fallback-src=\"https://github.githubassets.com/images/icons/emoji/unicode/1f3d6.png\">🏖️</g-emoji></div>",
-        false,
-        "Just a man, shipping code"
+        url = "https://github.com/jumaallan",
+        avatarUrl = "https://avatars2.githubusercontent.com/u/25085146?u=ce5247f3c267df4cdbe2af79efee9c848d1791e0&v=4",
+        bioHtml = "<div>Android @ Backbase</div>",
+        companyHtml = "<div>Backbase</div>",
+        email = "allanjuma@gmail.com",
+        followersTotalCount = 610,
+        followingTotalCount = 13,
+        isDeveloperProgramMember = false,
+        isVerified = true,
+        isEmployee = false,
+        isViewer = false,
+        location = "Nairobi",
+        login = "jumaallan",
+        name = "Juma Allan",
+        organizationsCount = 16,
+        repositoriesCount = 45,
+        starredRepositoriesCount = 108,
+        viewerCanFollow = false,
+        viewerIsFollowing = false,
+        websiteUrl = "https://androidstudy.com",
+        isOrganization = false,
+        emojiHtml = "<div><g-emoji class=\"g-emoji\" alias=\"beach_umbrella\" fallback-src=\"https://github.githubassets.com/images/icons/emoji/unicode/1f3d6.png\">🏖️</g-emoji></div>",
+        indicatesLimitedAvailability = false,
+        message = "Just a man, shipping code"
     )
 )
 
 val testFollower = listOf(
     Followers(
         0,
-        "jumaallan",
-        "akabiru",
-        "Kabiru Mwenja",
-        "https://avatars3.githubusercontent.com/u/17295175?u=554245ab33cd1c1d2d32c87064098c18c154ca0e&v=4",
-        "<div>Software Composer</div>"
+        userLogin = "jumaallan",
+        login = "akabiru",
+        name = "Kabiru Mwenja",
+        avatarUrl = "https://avatars3.githubusercontent.com/u/17295175?u=554245ab33cd1c1d2d32c87064098c18c154ca0e&v=4",
+        bioHtml = "<div>Software Composer</div>"
     )
 )
 
 val testFollowing = listOf(
     Followings(
         0,
-        "jumaallan",
-        "akabiru",
-        "Kabiru Mwenja",
-        "https://avatars3.githubusercontent.com/u/17295175?u=554245ab33cd1c1d2d32c87064098c18c154ca0e&v=4",
-        "<div>Software Composer</div>"
+        userLogin = "jumaallan",
+        login = "akabiru",
+        name = "Kabiru Mwenja",
+        avatarUrl = "https://avatars3.githubusercontent.com/u/17295175?u=554245ab33cd1c1d2d32c87064098c18c154ca0e&v=4",
+        bioHtml = "<div>Software Composer</div>"
     )
 )
